c2ef2a9fb11a07b0deb1cdb3124d13a288d70478
sylvansign <jake.d.bray@gmail.com>
2020-02-24 22:17:12 -0500

    websocket

--- server.pl
+++ server.pl
@@ -1,7 +1,25 @@
 :- use_module(library(http/websocket)).
-:- use_module(library(http/thread_httpd)).
-:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_server)).
 
+:- http_handler(root('.'),
+                meld,
+                []).
+:- http_handler(root(ws),
+                http_upgrade_to_websocket(echo, []),
+                []).
 
-server(Port) :-
-        http_server(http_dispatch, [port(Port)]).
+echo(WebSocket) :-
+    ws_receive(WebSocket, Message),
+    (   Message.opcode==close
+    ->  true
+    ;   ws_send(WebSocket, Message),
+        echo(WebSocket)
+    ).
+
+meld(_Request) :-
+    reply_html_page(
+        title('Demo server'),
+        [ h1('Hello World!')
+        ]).
+
+:- initialization http_server([port(3000)]).
